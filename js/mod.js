da37b6279c08fc18c56bce6673b3655121f4d649
tekina111 <166177882+tekina111@users.noreply.github.com>
2024-05-13 16:10:56 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,79 +1,73 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
-	modFiles: ["layers.js", "tree.js"],
+  name: "The Necromancer Tree",
+  id: "theNecromancerTree",
+  author: "Tekina111",
+  pointsName: "Black Magic",
+  modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
-	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
-}
+  discordName: "",
+  discordLink: "",
+  initialStartPoints: new Decimal(10), // Used for hard resets and new players
+  offlineLimit: 1, // In hours
+};
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
-}
+  num: "0.0",
+  name: "Literally nothing",
+};
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
 		- Added things.<br>
-		- Added stuff.`
+		- Added stuff.`;
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congratulations! You have reached the end and beaten this game, but for now...`;
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
-var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"];
 
-function getStartPoints(){
-    return new Decimal(modInfo.initialStartPoints)
+function getStartPoints() {
+  return new Decimal(modInfo.initialStartPoints);
 }
 
 // Determines if it should show points/sec
-function canGenPoints(){
-	return true
+function canGenPoints() {
+  return true;
 }
 
 // Calculate points/sec!
 function getPointGen() {
-	if(!canGenPoints())
-		return new Decimal(0)
+  if (!canGenPoints()) return new Decimal(0);
 
-	let gain = new Decimal(1)
-	return gain
+  let gain = new Decimal(1);
+  return gain;
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
-function addedPlayerData() { return {
-}}
+function addedPlayerData() {
+  return {};
+}
 
 // Display extra things at the top of the page
-var displayThings = [
-]
+var displayThings = [];
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+  return player.points.gte(new Decimal("e280000000"));
 }
-
-
 
 // Less important things beyond this point!
 
 // Style for the background, can be a function
-var backgroundStyle = {
-
-}
+var backgroundStyle = {};
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+  return 3600; // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
-function fixOldSave(oldVersion){
-}
+function fixOldSave(oldVersion) {}
